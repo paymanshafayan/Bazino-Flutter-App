d080b580a2426e4a4feca17c31531f99b95fdf0f
paymanshafayan <pshafayan@yahoo.com>
2026-08-01 09:37:06 +0330

    Change application ID and namespace for Android app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.bazino_app"
+    namespace = "com.bazino.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.bazino_app"
+        applicationId = "com.bazino.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
